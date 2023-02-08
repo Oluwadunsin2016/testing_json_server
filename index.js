3205fbb2afe3e93323d57cf0e7a65d3bdec221c6
Oluwadunsin <oluwadunsin2016@gmail.com>
2023-02-08 15:58:09 +0100

    Test json server

---
+++ index.js
@@ -0,0 +1,10 @@
+const jsonServer = require('json-server')
+const server = jsonServer.create()
+const router = jsonServer.router('blog.json')
+const middlewares = jsonServer.defaults()
+
+server.use(middlewares)
+server.use(router)
+server.listen(5005, () => {
+  console.log('JSON Server is running')
+})
